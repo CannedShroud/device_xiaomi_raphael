39752af2d69fe429eec64622c94a82c3d3943835
pavanpaps <pavanishot200@gmail.com>
2021-09-27 17:39:31 +0000

    [WIP] raphael: Prebuilt Cam Provider

--- device.mk
+++ device.mk
@@ -142,12 +142,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    libcamera2ndk_vendor \
     libdng_sdk.vendor \
-    libgui_vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.xiaomi.hardware.motor@1.0.vendor
 
 ifeq ($(ARROW_GAPPS), true)
