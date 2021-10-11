ba92c6f1cfda82c9ded447d797ca1ff5a3825b46
Arian <arian.kulmer@web.de>
2021-10-11 23:49:29 +0200

    raphael: weird fod stuff
    Change-Id: I4a4023fca503d5b0699c21359025b6165aa71e27

--- device.mk
+++ device.mk
@@ -223,8 +223,9 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_raphael \
+    android.hardware.biometrics.fingerprint@2.3-service.raphael \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+
 
 # GPS
 PRODUCT_COPY_FILES += \
