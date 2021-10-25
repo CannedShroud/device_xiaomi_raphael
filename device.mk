3b0a5a81d37757801360849266c644073602a8b8
CannedShroud <hypersync112@gmail.com>
2021-10-25 18:09:17 +0530

    Revert "raphael: move memlat settings to separate script"
    This reverts commit e7b60cfa19112885cb72acde5fff23a9f10c8526.

--- device.mk
+++ device.mk
@@ -270,7 +270,8 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
-    init.qti.dcvs.sh \
+    init.qcom.usb.sh \
+    init.recovery.qcom.rc \
     init.target.rc \
     init.xiaomi.rc \
     ueventd.qcom.rc
