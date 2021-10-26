d7bcb88cc4c79ebb862bcd2f20eeb5572a254c17
CannedShroud <hypersync112@gmail.com>
2021-10-26 18:26:14 +0530

    raphael: device: ugh missing separator

--- device.mk
+++ device.mk
@@ -421,7 +421,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.xiaomi_msmnile
+    android.hardware.thermal@2.0-service.xiaomi_msmnile \
     androdi.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
